7b87235ea0deb80e0fcb4bd82f15cd43711c1b71
Andrew Johnson <itsandyjohn@gmail.com>
2022-05-10 23:48:27 -0600

    added favicon

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,7 @@ import IsSearchingProvider from '../components/providers/IsSearchingProvider';
 import RaceListProvider from '../components/providers/RaceListProvider';
 import RaceEventsProvider from '../components/providers/RaceEventsProvider';
 import ErrorBoundary from '../components/ErrorBoundary';
+import Head from 'next/head';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
@@ -17,6 +18,33 @@ Router.events.on('routeChangeError', () => NProgress.done());
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        ></link>
+        <link rel="manifest" href="/site.webmanifest"></link>
+        <link
+          rel="mask-icon"
+          href="/safari-pinned-tab.svg"
+          color="#5bbad5"
+        ></link>
+        <meta name="msapplication-TileColor" content="#da532c"></meta>
+        <meta name="theme-color" content="#ffffff"></meta>
+      </Head>
       <Header />
       <RaceListProvider>
         <RaceEventsProvider>
